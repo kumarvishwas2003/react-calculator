648813230cc757e323d56cbb1d963e07ed2d7391
vishwas <vishwas452003@gmail.com>
2024-07-29 14:00:59 +0530

    calculator complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import { useState } from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import { Switch } from '@chakra-ui/react'
-import './App.css'
+import { useState } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Switch } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
-  const [darkMode,setDarkMode] = useState(false)
-  const [value,setValue] = useState(0)
-  
-  const toggleDark = ()=>{
-    setDarkMode(!darkMode)
-    if(darkMode){
-      document.documentElement.classList.remove("dark")
+  const [darkMode, setDarkMode] = useState(false);
+  const [value, setValue] = useState("");
+
+  const toggleDark = () => {
+    setDarkMode(!darkMode);
+    if (darkMode) {
+      document.documentElement.classList.remove("dark");
+    } else {
+      document.documentElement.classList.add("dark");
     }
-    else{
-      document.documentElement.classList.add("dark")
-    }
-  }
-  const change = ()=>{
-    console.log("hello")
-  }
+  };
+  const change = () => {
+    console.log("hello");
+  };
 
   return (
     <>
       <ChakraProvider>
-        <div className="h-screen w-screen bg-white flex flex-col justify-center items-center dark:bg-[rgb(29,41,57)] text-xl">
+        <div className="h-screen w-screen bg-white flex flex-col justify-center items-center dark:bg-[rgb(29,41,57)] text-xl transition delay-75">
           <div className="switch flex w-full md:w-96 justify-end">
             <Switch id="email-alerts" className="" onChange={toggleDark} />
           </div>
@@ -31,68 +30,176 @@ function App() {
             <div className="input bg-white col-span-3">
               <input
                 type="text"
-                className="bg-slate-50 w-full h-full shadow-sm dark:bg-[rgb(29,41,57)]"
+                className="bg-slate-50 w-full h-full shadow-sm dark:bg-[rgb(29,41,57)] text-right text-4xl font-bold dark:text-white px-2"
                 placeholder="0"
-                value={99}
+                value={value}
               />
             </div>
-            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+            <button
+              className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white"
+              onClick={(e) => {
+                setValue("");
+              }}
+            >
               AC
             </button>
-            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+            <button
+              className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white"
+              value={"+"}
+              onClick={(e) => {
+                setValue(value + e.target.value);
+              }}
+            >
               +
             </button>
-            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+            <button
+              className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white"
+              value={"-"}
+              onClick={(e) => {
+                setValue(value + e.target.value);
+              }}
+            >
               -
             </button>
 
-            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+            <button
+              className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white"
+              value={"*"}
+              onClick={(e) => {
+                setValue(value + e.target.value);
+              }}
+            >
               x
             </button>
-            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+            <button
+              className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white"
+              value={"/"}
+              onClick={(e) => {
+                setValue(value + e.target.value);
+              }}
+            >
               /
             </button>
-            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+            <button
+              className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white"
+              value={"."}
+              onClick={(e) => {
+                setValue(value + e.target.value);
+              }}
+            >
               .
             </button>
 
-            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+            <button
+              className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white"
+              value={7}
+              onClick={(e) => {
+                setValue(value + e.target.value);
+              }}
+            >
               7
             </button>
-            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+            <button
+              className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white"
+              value={8}
+              onClick={(e) => {
+                setValue(value + e.target.value);
+              }}
+            >
               8
             </button>
-            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+            <button
+              className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white"
+              value={9}
+              onClick={(e) => {
+                setValue(value + e.target.value);
+              }}
+            >
               9
             </button>
 
-            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+            <button
+              className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white"
+              value={4}
+              onClick={(e) => {
+                setValue(value + e.target.value);
+              }}
+            >
               4
             </button>
-            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+            <button
+              className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white"
+              value={5}
+              onClick={(e) => {
+                setValue(value + e.target.value);
+              }}
+            >
               5
             </button>
-            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+            <button
+              className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white"
+              value={6}
+              onClick={(e) => {
+                setValue(value + e.target.value);
+              }}
+            >
               6
             </button>
 
-            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+            <button
+              className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white"
+              value={1}
+              onClick={(e) => {
+                setValue(value + e.target.value);
+              }}
+            >
               1
             </button>
-            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+            <button
+              className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white"
+              value={2}
+              onClick={(e) => {
+                setValue(value + e.target.value);
+              }}
+            >
               2
             </button>
-            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+            <button
+              className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white"
+              value={3}
+              onClick={(e) => {
+                setValue(value + e.target.value);
+              }}
+            >
               3
             </button>
 
-            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+            <button
+              className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white"
+              value={0}
+              onClick={(e) => {
+                setValue(value + e.target.value);
+              }}
+            >
               0
             </button>
-            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+            <button
+              className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white"
+              value={"00"}
+              onClick={(e) => {
+                setValue(value + e.target.value);
+              }}
+            >
               00
             </button>
-            <button className="button bg-red-500">=</button>
+            <button
+              className="button bg-red-500"
+              onClick={(e) => {
+                setValue(eval(value));
+              }}
+            >
+              =
+            </button>
           </div>
         </div>
       </ChakraProvider>
@@ -100,4 +207,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
