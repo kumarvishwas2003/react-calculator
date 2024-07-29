d0225f3cb9fc51f469aa9b0b8921f20269cc1746
vishwas <vishwas452003@gmail.com>
2024-07-29 11:29:31 +0530

    change tailsind button to normal css to reduce the size

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import './App.css'
 
 function App() {
   const [darkMode,setDarkMode] = useState(false)
-
+  const [value,setValue] = useState(0)
+  
   const toggleDark = ()=>{
     setDarkMode(!darkMode)
     if(darkMode){
@@ -21,45 +22,82 @@ function App() {
 
   return (
     <>
-  <ChakraProvider>
-    <div className="h-screen w-screen bg-white flex flex-col justify-center items-center dark:bg-[rgb(29,41,57)] text-xl">
-      <div className="switch flex w-full md:w-96 justify-end">
-        <Switch id="email-alerts" className="" onChange={toggleDark} />
-      </div>
-      <div className="calculator border dark:bg-[rgb(14,23,40)] w-full md:w-96 grid grid-cols-3 grid-rows-7 p-4 gap-2">
-        <div className="input bg-white col-span-3">
-        <input type="text" className='bg-slate-50 w-full h-full shadow-sm dark:bg-[rgb(29,41,57)]' placeholder='0'/>
+      <ChakraProvider>
+        <div className="h-screen w-screen bg-white flex flex-col justify-center items-center dark:bg-[rgb(29,41,57)] text-xl">
+          <div className="switch flex w-full md:w-96 justify-end">
+            <Switch id="email-alerts" className="" onChange={toggleDark} />
+          </div>
+          <div className="calculator border dark:bg-[rgb(14,23,40)] w-full md:w-96 grid grid-cols-3 grid-rows-7 p-4 gap-2">
+            <div className="input bg-white col-span-3">
+              <input
+                type="text"
+                className="bg-slate-50 w-full h-full shadow-sm dark:bg-[rgb(29,41,57)]"
+                placeholder="0"
+                value={99}
+              />
+            </div>
+            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+              AC
+            </button>
+            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+              +
+            </button>
+            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+              -
+            </button>
+
+            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+              x
+            </button>
+            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+              /
+            </button>
+            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+              .
+            </button>
+
+            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+              7
+            </button>
+            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+              8
+            </button>
+            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+              9
+            </button>
+
+            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+              4
+            </button>
+            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+              5
+            </button>
+            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+              6
+            </button>
+
+            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+              1
+            </button>
+            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+              2
+            </button>
+            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+              3
+            </button>
+
+            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+              0
+            </button>
+            <button className="button bg-slate-50 dark:bg-[rgb(29,41,57)] dark:text-white">
+              00
+            </button>
+            <button className="button bg-red-500">=</button>
+          </div>
         </div>
-  <button className='bg-slate-50 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg dark:bg-[rgb(29,41,57)] dark:text-white'>AC</button>
-  <button className='bg-slate-50 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg dark:bg-[rgb(29,41,57)] dark:text-white'>+</button>
-  <button className='bg-slate-50 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg dark:bg-[rgb(29,41,57)] dark:text-white'>-</button>
-
-  <button className='bg-slate-50 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg dark:bg-[rgb(29,41,57)] dark:text-white '>x</button>
-  <button className='bg-slate-50 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg dark:bg-[rgb(29,41,57)] dark:text-white '>/</button>
-  <button className='bg-slate-50 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg dark:bg-[rgb(29,41,57)] dark:text-white '>.</button>
-
-  <button className='bg-slate-50 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg dark:bg-[rgb(29,41,57)] dark:text-white'>7</button>
-  <button className='bg-slate-50 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg dark:bg-[rgb(29,41,57)] dark:text-white'>8</button>
-  <button className='bg-slate-50 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg dark:bg-[rgb(29,41,57)] dark:text-white'>9</button>
-
-  <button className='bg-slate-50 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg dark:bg-[rgb(29,41,57)] dark:text-white'>4</button>
-  <button className='bg-slate-50 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg dark:bg-[rgb(29,41,57)] dark:text-white'>5</button>
-  <button className='bg-slate-50 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg dark:bg-[rgb(29,41,57)] dark:text-white'>6</button>
-
-  <button className='bg-slate-50 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg dark:bg-[rgb(29,41,57)] dark:text-white'>1</button>
-  <button className='bg-slate-50 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg dark:bg-[rgb(29,41,57)] dark:text-white'>2</button>
-  <button className='bg-slate-50 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg dark:bg-[rgb(29,41,57)] dark:text-white'>3</button>
-
-  <button className='bg-slate-50 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg dark:bg-[rgb(29,41,57)] dark:text-white'>0</button>
-  <button className='bg-slate-50 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg dark:bg-[rgb(29,41,57)] dark:text-white'>00</button>
-  <button className='bg-red-500 rounded-full w-16 h-16 flex items-center justify-center ml-4 mt-3 font-bold shadow-lg'>=</button>
-</div>
-
-      </div>
-  </ChakraProvider>
-</>
-
-  )
+      </ChakraProvider>
+    </>
+  );
 }
 
 export default App
